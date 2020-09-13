bec4f221bb21bb174d23f72d70ef9d7ec6981237
Dike Pascal Ozioma <pascalozioma18@gmail.com>
2020-09-14 00:35:18 +0100

    updated AuthController

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -56,6 +56,6 @@ class AuthController extends Controller
     public function logout(Request $request, Response $response)
     {
         Application::$app->logout();
-        $response->redirect('/index.html');
+        $response->redirect('/');
     }
 }
